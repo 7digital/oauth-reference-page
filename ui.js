8c165e38f4aa2c35fba4939b1dc4d310a59f2e50
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-01-22 15:15:58 +0000

    HEAD HTTP Verb should be available for generating the signature

--- ui.js
+++ ui.js
@@ -68,7 +68,7 @@
             return self.parameters.nonce(Math.floor(Math.random() * 1e9).toString());
         };
         self.newNonce();
-        self.methodOptions = ko.observableArray([ "GET", "POST", "PUT", "DELETE" ]);
+        self.methodOptions = ko.observableArray([ "GET", "POST", "PUT", "DELETE", "HEAD" ]);
         self.encodingOptions = ko.observableArray([ "application/json", "application/xml" ]);
         self.oauthSignature = ko.computed(function() {
             return oauthSigner(self.parameters);
